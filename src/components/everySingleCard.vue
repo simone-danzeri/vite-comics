53b7ebb3f4893536dc9518acb40d761a0b90708c
simone-danzeri <s.danzeri@icloud.com>
2024-04-12 13:49:56 +0200

    feat: add cards (alcune non si vedono per qualche motivo)

---
+++ src/components/everySingleCard.vue
@@ -0,0 +1,39 @@
+<script>
+    export default {
+        name: 'everySingleCard',
+        props: {
+            titleInfo: Object
+        }
+    }
+</script>
+
+
+<template>
+        <div class="card">
+            <img :src="titleInfo.thumb" alt="">
+            <span>{{ titleInfo.series }}</span>
+        </div>
+</template>
+
+
+<style scoped lang="scss">
+@use '../style/partials/variables' as *;
+.card{
+    display: flex;
+    flex-direction: column;
+    width: calc(100%/6 - 20px);
+    padding: 10px;
+    margin: 10px;
+
+    img{
+        width: 100%;
+        aspect-ratio: 1/1;
+    }
+
+    span{
+        color: white;
+        text-align: center;
+        margin-top: 10px;
+    }
+}
+</style>
